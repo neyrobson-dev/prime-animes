6898efd512d29c4aebe7539503401278d4b7d2f2
Ney Robson <ney.robson@kbank.com.br>
2021-01-13 10:26:22 -0300

    ajustes

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -31,7 +31,10 @@ const Search: React.FC = () => {
         if (value.length > 2) {
             setLoading(true);
             await api.get(`/api-animesbr-10.php?search=${value}`).then(response => {
-                setAnimes(response.data);
+                if (!response.data)
+                    setAnimes([]);
+                else
+                    setAnimes(response.data);
                 setLoading(false);
             });
         }
@@ -48,35 +51,8 @@ const Search: React.FC = () => {
         }
     }
 
-    if (loading) {
-        return(
-            <Wrapper>
-                <Container>
-                    <Header>
-                        <Feather name="search" size={20} color="#e2e2e2" />
-                        <InputSearch
-                            placeholder="Busque por nome"
-                            placeholderTextColor="#e2e2e2"
-                            value={searchText}
-                            // onChangeText={onChangeText}
-                            onChangeText={(value) => {
-                                setSearchText(value)
-                                onChangeText(searchText)
-                            }}
-                        />
-                        {searchText != '' && (<Feather name="x" size={20} color="#e2e2e2" onPress={() => clearData()}></Feather>)}
-                    </Header>
-                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#000000'}}>
-                        <ActivityIndicator size="large" color="#e2e2e2" />
-                    </View>
-                </Container>
-            </Wrapper>
-        );
-    }
-
     return (
         <Wrapper>
-            <Container>
             <Header>
                 <Feather name="search" size={20} color="#e2e2e2" />
                 <InputSearch
@@ -91,14 +67,22 @@ const Search: React.FC = () => {
                 />
                 {searchText != '' && (<Feather name="x" size={20} color="#e2e2e2" onPress={() => clearData()}></Feather>)}
             </Header>
-            <LatestList>
-                {animes.map((item) => (
-                <LatestItem key={item.id} onPress={() => {navigateToAnimeDetail(item.id)}}>
-                    <LatestItemImage source={{ uri: `http://cdn.appanimeplus.tk/img/${item.category_image}`, width: 135, height: 189 }} />
-                    <LatestItemTitle>{ ((item.category_name).length > 35) ? (((item.category_name).substring(0, 35-3)) + '...') : item.category_name }</LatestItemTitle>
-                </LatestItem>
-                ))}
-            </LatestList>
+            <Container>
+                {loading && (
+                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#000000'}}>
+                        <ActivityIndicator size="large" color="#e2e2e2" />
+                    </View>
+                )}
+                {!loading && (
+                <LatestList>
+                    {animes.map((item) => (
+                        <LatestItem key={item.id} onPress={() => {navigateToAnimeDetail(item.id)}}>
+                            <LatestItemImage source={{ uri: `http://cdn.appanimeplus.tk/img/${item.category_image}`, width: 135, height: 189 }} />
+                            <LatestItemTitle>{ ((item.category_name).length > 35) ? (((item.category_name).substring(0, 35-3)) + '...') : item.category_name }</LatestItemTitle>
+                        </LatestItem>
+                    ))}
+                </LatestList>
+                )}
             </Container>
         </Wrapper>
     );
