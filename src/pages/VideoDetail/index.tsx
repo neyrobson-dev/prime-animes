e6f642299e9bd1c83c1383be38ca9c2a7dced839
Ney Robson <ney.robson@kbank.com.br>
2021-06-30 10:20:56 -0300

    ajuste com token e parametro r

--- src/pages/VideoDetail/index.tsx
+++ src/pages/VideoDetail/index.tsx
@@ -50,9 +50,26 @@ const VideoDetail: React.FC = () => {
 
   useEffect(() => {
     async function loadStream(): Promise<void> {
-      await axios.get(`https://appanimeplus.tk/meuanimetv-40.php?episodios=${epsodeId}&token=${STREAMING_DATA_TOKEN}&r=${STREAMING_DATA_R}`).then((response: any) => {
+      const streamingDataR = Math.floor(Math.random() * 90000) + 10000;
+      const timeToMatch = (Date.now() / 1000) * 2;
+      const streamingDataToken = (timeToMatch * streamingDataR).toFixed();
+      // await axios.get(`https://appanimeplus.tk/meuanimetv-40.php?episodios=${epsodeId}&token=${STREAMING_DATA_TOKEN}&r=${STREAMING_DATA_R}`).then((response: any) => {
+      //   setStream(response.data[0]);
+      // });
+      await axios.get('https://appanimeplus.tk/meuanimetv-40.php', {
+        params: {
+          episodios: epsodeId,
+          token: streamingDataToken,
+          r: streamingDataR,
+        },
+        headers: {
+          'X-Auth': 'VnM4ejB1dElLajZDZDhiSU02aGF0RmdDQWxXNDl3SEQzWjE2Ulg1K3ZOWjZkbjJXZjBqT2xnT0FVdnVwd2VjTEdkaS9KM0VlaHM4L1psc1J4R25SWHVpTGdjMVBvYUViTExhQXZpMzZGTytiVjh0SnQzUDVxL3ZVTDVZdkYwSDE1bFdFV1V4WGRwbUFFM0NJNjJOWEkvSndhbFVjZXNZYVRROGFKK2lkT2lEaU9nMElQT0hWTFlxNzlHajNPSVNhSjdnUDZTeldtbHVRaXlxMk5qSkFKV09NbDBIQkZhVTJYMitaaGtrMTd1OVJlSE5saVlGdUJOMk5TSGlBbzByYjFGeDN5N0Q2eHhjZFdzWS90b1JacFJtRGJ5QVdXLzBlTmhKK3UvVElGa3Q4T1Q5ODFZQjBzN0gxQVArZnY5NGo=',
+        }
+      }).then((response: any) => {
         setStream(response.data[0]);
-      });
+      }).catch((error) => {
+        console.log(error);
+      })
     }
 
     loadStream();    
